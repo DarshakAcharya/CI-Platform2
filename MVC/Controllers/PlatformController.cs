e93b49be49bcc6c453019362e58e2c60cc56d16c
DarshakAcharya <darshakacharya2625@gmail.com>
2023-02-20 17:54:42 +0530

    MVC

---
+++ MVC/Controllers/PlatformController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class PlatformController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
